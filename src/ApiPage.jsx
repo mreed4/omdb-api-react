d4e2e73ba6a9ab4adfb85412c95d221a2a3ba56b
Matthew <78713317+mreed4@users.noreply.github.com>
2023-04-08 10:53:15 -0700

    New page

---
+++ src/ApiPage.jsx
@@ -0,0 +1,8 @@
+export default function ApiPage() {
+  return (
+    <section id="api">
+      <h1>API Key</h1>
+      <hr />
+    </section>
+  );
+}
---
+++ src/ContactPage.jsx
@@ -0,0 +1,8 @@
+export default function ContactPage() {
+  return (
+    <section id="contact">
+      <h1>Contact</h1>
+      <hr />
+    </section>
+  );
+}
