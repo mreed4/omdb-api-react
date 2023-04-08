d88ea61469fc29985ee21d9774bfdddf017ada59
Matthew <78713317+mreed4@users.noreply.github.com>
2023-04-08 16:45:19 -0700

    Add footer

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import { AppProvider } from "./AppContext";
 import Nav from "./Nav";
 import Home from "./Pages/Home";
@@ -7,6 +7,7 @@ import DonatePage from "./Pages/DonatePage";
 import DemoPage from "./Pages/DemoPage";
 import ConnectPage from "./Pages/ConnectPage";
 import DashboardPage from "./Pages/DashboardPage";
+import Footer from "./Footer";
 // import "./App.css";
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
             <Route path="/connect" element={<ConnectPage />}></Route>
           </Routes>
         </main>
+        <Footer />
       </Router>
     </AppProvider>
   );
